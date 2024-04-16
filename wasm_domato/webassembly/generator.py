486617168d9b7c20425503dbce13b7d2b70138d7
happytraveller-alone <happytraveller@163.com>
2024-04-16 16:06:34 +0800

    add function support test jsc and spidermonkey, support store test results in file, support add develop shell run args

--- wasm_domato/webassembly/generator.py
+++ wasm_domato/webassembly/generator.py
@@ -3,7 +3,6 @@ import os
 import sys
 from grammar import Grammar
 import subprocess
-
 """
 Parses grammar rules from string.
 
@@ -75,165 +74,275 @@ def generate_samples(grammar_dir, \
             print('Writing a sample to ' + outfile)
             try:
                 f1 = open(os.path.join(output_merge_dir, outfile), 'w')
-                f2 = open(os.path.join(input_dir, 'wasm-module-builder.js'), 'r')
+                f2 = open(os.path.join(input_dir, 'wasm-module-builder.js'),
+                          'r')
                 f1.write(f2.read())
                 f2.close()
                 f1.write(result)
                 f1.close()
-                
+
             except IOError:
                 print('Error writing to output')
     return
 
 
-def get_test_cases_v8_output(output_dir, enable_test, outfiles):
-    if enable_test == 'On':
-        print('Test cases are enabled in v8')
-        v8_executable = "/home/xyf/v8/v8/out/CVE_2024_2887_debug_tag_12.4.176/d8"
-        for outfile in outfiles:
-            wasm_file = os.path.join(output_dir, outfile)
-            command = v8_executable + "\t"  + wasm_file
-            result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            # Get the return code
-            return_code = result.returncode
-            if(return_code == 11):
-                print('Segmentation fault')
-                # with open
-            # Get the output
-            output = result.stdout.decode()
-            # Get the error message (if any)
-            error = result.stderr.decode()
-            # Print the return code, output, and error message
-            print(f"Return code: {return_code}")
-            print(f"Output: {output}")
-            print(f"Error: {error}")
-    if enable_test == 'Off':
-        print('Test cases are disabled in v8')
+def get_test_results(dir_path_map, outfiles, develop_shell, base_dir,
+                     develop_shell_args):
+    outfiles_crash = [
+        os.path.join(base_dir, f'fuzz-{i:05d}-record.txt')
+        for i in range(len(outfiles))
+    ]
+    for outfile in outfiles:
+        wasm_file = os.path.join(dir_path_map['output_merge_dir'], outfile)
+        command = develop_shell + " " + develop_shell_args + "\t" + wasm_file
+        result = subprocess.run(command,
+                                shell=True,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        return_code = result.returncode
+        with open(outfiles_crash[outfiles.index(outfile)], 'w') as f:
+            f.write(f"Command: {command}\n\n")
+            f.write(f"Return code: {return_code}\n\n")
+            f.write(f"Output: {result.stdout.decode()}\n\n")
+            f.write(f"Error: {result.stderr.decode()}\n")
+            f.close()
 
 
 def print_usage():
     print("Usage:")
     # print("\tpython generator.py <output file>")
-    # print("\tpython generator.py <output file> --enable_test <On/Off>")
+    # print("\tpython generator.py <output file> --enable_test <On/False>")
     print("\tpython generator.py --output_dir <dir> --no_of_files <count>")
-    print("\tpython generator.py --output_dir <dir> --no_of_files <count> --enable_test <On/Off>")
+    print(
+        "\tpython generator.py --output_dir <dir> --no_of_files <count> --enable_test <On/False>"
+    )
+    print("build v8 command:")
     sys.exit(1)
 
-def validate_enable_test_option(option):
-    if option not in ["On", "Off"]:
-        print("Invalid argument for test cases. Please enter 'On' or 'Off' for enable_test command.")
-        print_usage()
-        sys.exit(2)
+
+# def validate_enable_test_option(option):
+#     if option not in ["On", "False"]:
+#         print(
+#             "Invalid argument for test cases. Please enter 'On' or 'False' for enable_test command."
+#         )
+#         print_usage()
+#         sys.exit(2)
 
 
+def generate_samples_name(dir_path_map, out_dir, file_count):
+    outfiles = [
+        os.path.join(out_dir, f'fuzz-{i:05d}.js') for i in range(file_count)
+    ]
+    generate_samples(dir_path_map['grammar_dir'], dir_path_map['input_dir'],
+                     out_dir, outfiles)
+    # if enable_test == 'On':
+    #     get_test_cases_v8_output(dir_path_map, enable_test, outfiles)
 
-def generate_samples_name(dir_path_map, out_dir, file_count, enable_test):
-
-    outfiles = [os.path.join(out_dir, f'fuzz-{i:05d}.js') for i in range(file_count)]
-
-    generate_samples(dir_path_map['grammar_dir'], dir_path_map['input_dir'], out_dir, outfiles)
-    
-    if enable_test == 'On':
-        get_test_cases_v8_output(out_dir, enable_test, outfiles)
 
 def check_arguments(dir_path_map, arg_map):
     if '--help' in sys.argv:
         print_usage()
         sys.exit(0)
-    for key, value in arg_map.items():
-        if key in sys.argv:
-            index = sys.argv.index(key)
-            try:
-                arg_map[key] = sys.argv[index + 1]
-            except IndexError:
-                print(f"Missing value for {key}")
-                print_usage()
     if '--enable_test' in sys.argv:
-        validate_enable_test_option(arg_map['--enable_test'])
-        print('enable_test is set to ' + arg_map['--enable_test'])
+        # validate_enable_test_option(arg_map['--enable_test'])
+        arg_map['--enable_test'] = True
+        print('enable_test is set to ' + str(arg_map['--enable_test']))
+        arg_map['--enable_test_v8'] = True
+        arg_map['--enable_test_jsc'] = True
+        arg_map['--enable_test_sm'] = True
     else:
-        print('enable_test is not set, default to Off')
+        print('enable_test is not set, default to False')
+        if '--enable_test_v8' in sys.argv:
+            # validate_enable_test_option(arg_map['--enable_test_v8'])
+            arg_map['--enable_test_v8'] = True
+            print('enable_test_v8 is set to ' + str(arg_map['--enable_test_v8']))
+        else:
+            print('enable_test_v8 is not set, default to False')
+        if '--enable_test_jsc' in sys.argv:
+            arg_map['--enable_test_jsc'] = True
+            # validate_enable_test_option(arg_map['--enable_test_jsc'])
+            print('enable_test_jsc is set to ' + str(arg_map['--enable_test_jsc']))
+        else:
+            print('enable_test_jsc is not set, default to False')
+        if '--enable_test_sm' in sys.argv:
+            arg_map['--enable_test_sm'] = True
+            # validate_enable_test_option(arg_map['--enable_test_sm'])
+            print('enable_test_sm is set to ' + str(arg_map['--enable_test_sm']))
+        else:
+            print('enable_test_sm is not set, default to False')
     if '--no_of_files' in sys.argv:
-        arg_map['--no_of_files'] = int(arg_map['--no_of_files'])
+        arg_map['--no_of_files'] = int( sys.argv[sys.argv.index('--no_of_files') + 1])
         print('no_of_files is set to ' + str(arg_map['--no_of_files']))
     else:
         print('no_of_files is not set, default to 1')
     if '--output_dir' in sys.argv:
+        arg_map['--output_dir'] = sys.argv[sys.argv.index('--output_dir') + 1]
         if not os.path.isabs(arg_map['--output_dir']):
-            arg_map['--output_dir'] = os.path.join(dir_path_map['fuzz_dir'], arg_map['--output_dir'])
+            arg_map['--output_dir'] = os.path.join(dir_path_map['fuzz_dir'],
+                                                   arg_map['--output_dir'])
         if not os.path.exists(arg_map['--output_dir']):
             os.makedirs(arg_map['--output_dir'])
         print('output_dir is set to ' + arg_map['--output_dir'])
     else:
         print('output_dir is not set, default to output_merge')
-        # arg_map['--output_dir'] = dir_path_map['output_merge_dir']
-    
+
+
+def clean_dir(dir_path_map):
+    if os.path.exists(dir_path_map['output_merge_dir']):
+        for file in os.listdir(dir_path_map['output_merge_dir']):
+            os.remove(os.path.join(dir_path_map['output_merge_dir'], file))
+    else:
+        os.makedirs(dir_path_map['output_merge_dir'])
+    if os.path.exists(dir_path_map['useful_crash_v8']):
+        for file in os.listdir(dir_path_map['useful_crash_v8']):
+            os.remove(os.path.join(dir_path_map['useful_crash_v8'], file))
+    else:
+        os.makedirs(dir_path_map['useful_crash_v8'])
+    if os.path.exists(dir_path_map['useful_crash_jsc']):
+        for file in os.listdir(dir_path_map['useful_crash_jsc']):
+            os.remove(os.path.join(dir_path_map['useful_crash_jsc'], file))
+    else:
+        os.makedirs(dir_path_map['useful_crash_jsc'])
+    if os.path.exists(dir_path_map['useful_crash_sm']):
+        for file in os.listdir(dir_path_map['useful_crash_sm']):
+            os.remove(os.path.join(dir_path_map['useful_crash_sm'], file))
+    else:
+        os.makedirs(dir_path_map['useful_crash_sm'])
+
+
+def test_samples(dir_path_map, args_map, develop_shell_map,
+                 develop_shell_args_map):
+    if args_map['--enable_test'] == True:
+        get_test_results(dir_path_map,
+                         os.listdir(dir_path_map['output_merge_dir']),
+                         develop_shell_map['develop_shell_v8'],
+                         dir_path_map['useful_crash_v8'],
+                         develop_shell_args_map['develop_shell_v8'])
+        get_test_results(dir_path_map,
+                         os.listdir(dir_path_map['output_merge_dir']),
+                         develop_shell_map['develop_shell_jsc'],
+                         dir_path_map['useful_crash_jsc'],
+                         develop_shell_args_map['develop_shell_jsc'])
+        get_test_results(dir_path_map,
+                         os.listdir(dir_path_map['output_merge_dir']),
+                         develop_shell_map['develop_shell_sm'],
+                         dir_path_map['useful_crash_sm'],
+                         develop_shell_args_map['develop_shell_sm'])
+    if args_map['--enable_test'] == False:
+        if args_map['--enable_test_v8'] == True:
+            get_test_results(dir_path_map,
+                             os.listdir(dir_path_map['output_merge_dir']),
+                             develop_shell_map['develop_shell_v8'],
+                             dir_path_map['useful_crash_v8'],
+                             develop_shell_args_map['develop_shell_v8'])
+        if args_map['--enable_test_jsc'] == True:
+            get_test_results(dir_path_map,
+                             os.listdir(dir_path_map['output_merge_dir']),
+                             develop_shell_map['develop_shell_jsc'],
+                             dir_path_map['useful_crash_jsc'],
+                             develop_shell_args_map['develop_shell_jsc'])
+        if args_map['--enable_test_sm'] == True:
+            get_test_results(dir_path_map,
+                             os.listdir(dir_path_map['output_merge_dir']),
+                             develop_shell_map['develop_shell_sm'],
+                             dir_path_map['useful_crash_sm'],
+                             develop_shell_args_map['develop_shell_sm'])
 
 def main():
     fuzz_dir = os.path.dirname(__file__)
-    # 以上路径整理在map中
+    # 路径整理
     dir_path_map = {
         'fuzz_dir': fuzz_dir,
         'grammar_dir': os.path.join(fuzz_dir, 'grammar'),
-        'output_orig_dir': os.path.join(fuzz_dir, 'output_orig'),
+        'useful_crash_v8': os.path.join(fuzz_dir, 'useful_crash_v8'),
+        'useful_crash_jsc': os.path.join(fuzz_dir, 'useful_crash_jsc'),
+        'useful_crash_sm': os.path.join(fuzz_dir, 'useful_crash_sm'),
         'input_dir': os.path.join(fuzz_dir, 'input'),
         'output_merge_dir': os.path.join(fuzz_dir, 'output_merge')
     }
+
     # 参数map
     arg_map = {
         '--output_dir': dir_path_map['output_merge_dir'],
         '--no_of_files': 1,
-        '--enable_test': 'Off',
-        '--help': False
+        '--enable_test': False,
+        '--enable_test_v8': False,
+        '--enable_test_jsc': False,
+        '--enable_test_sm': False,
+        '--help': False,
+        '--help_build_v8': False,
+        '--help_build_jsc': False,
+        '--help_build_sm': False,
     }
     # 参数填充
     check_arguments(dir_path_map, arg_map)
+    # 清空文件夹
+    clean_dir(dir_path_map)
     # 生成样本
-    generate_samples_name(dir_path_map, arg_map['--output_dir'], arg_map['--no_of_files'], arg_map['--enable_test'])
+    generate_samples_name(dir_path_map, arg_map['--output_dir'],
+                          arg_map['--no_of_files'])
+    # 测试样本
+    develop_shell_map = {
+        'develop_shell_v8':
+        "/home/xyf/v8/v8/out/v8_12.5.169/d8",
+        'develop_shell_jsc':
+        "/home/xyf/WebKit/20240416_fuzz_commit_939e7000670a9204a8b03f31f9ebd252d7486a1f/JSCOnly/Release/bin/jsc",
+        'develop_shell_sm':
+        "/home/xyf/spidermonkey/js/src/20240416_fuzz_commit_23ad6bdb310c9a46e6f37885b0d88ec5da3c00f7/dist/bin/js",
+    }
+    # 测试样本参数
+    develop_shell_args_map = {
+        'develop_shell_v8':
+        "--turboshaft-wasm \
+            --allow-natives-syntax \
+            --turboshaft-wasm-load-elimination \
+            --turboshaft-wasm-instruction-selection-experimental \
+            --turboshaft-wasm-instruction-selection-staged \
+            --wasm-deopt \
+            --experimental-wasm-compilation-hints \
+            --experimental-wasm-instruction-tracing \
+            --experimental-wasm-assume-ref-cast-succeeds \
+            --experimental-wasm-ref-cast-nop \
+            --experimental-wasm-branch-hinting \
+            --experimental-wasm-stack-switching \
+            --experimental-wasm-jspi \
+            --experimental-wasm-shared \
+            --experimental-wasm-type-reflection \
+            --experimental-wasm-memory64 \
+            --experimental-wasm-stringref \
+            --experimental-wasm-imported-strings \
+            --experimental-wasm-inlining \
+            --experimental-wasm-exnref \
+            --experimental-wasm-multi-memory \
+            --experimental-wasm-legacy-eh \
+            --wasm-staging \
+            --experimental-wasm-revectorize",
+        'develop_shell_jsc':
+        "--useWebAssemblyThreading=true \
+            --useWebAssemblyTypedFunctionReferences=true \
+            --useWebAssemblyGC=true \
+            --useWebAssemblySIMD=true \
+            --useWebAssemblyRelaxedSIMD=true \
+            --useWebAssemblyTailCalls=true \
+            --useWebAssemblyExtendedConstantExpressions=true \
+            --useWasmLLInt=true \
+            --useWebAssembly=true \
+            --wasmLLIntTiersUpToBBQ=true",
+        'develop_shell_sm':
+        "--wasm-compiler=baseline+optimizing \
+            --setpref=wasm_exnref=true \
+            --setpref=wasm_gc=true \
+            --setpref=wasm_js_string_builtins=true \
+            --setpref=wasm_memory_control=true \
+            --setpref=wasm_moz_intgemm=true \
+            --setpref=wasm_multi_memory=true \
+            --setpref=wasm_relaxed_simd=true \
+            --setpref=wasm_tail_calls=true \
+            --setpref=wasm_test_serialization=true",
+    }
+    test_samples(dir_path_map, arg_map, develop_shell_map,
+                 develop_shell_args_map)
 
-    
 
 if __name__ == '__main__':
     main()
-
-
-# """
-# Merges the contents of two files into a new file.
-# Args:
-#     file_module_builder (str): The path to the first file.
-#     file_wasm_test (str): The path to the second file.
-#     file_merged (str): The path to the merged file.
-# Returns:
-#     None
-# """
-# def merge_files(file_module_builder, \
-#                 file_wasm_test, \
-#                 file_merged):
-
-#     with open(file_module_builder, 'r') as f1, \
-#          open(file_wasm_test, 'r') as f2, \
-#          open(file_merged, 'w') as f3:
-#         f3.write(f1.read())
-#         f3.write(f2.read())
-#         f3.close()
-#     print(
-#         f"Files {file_module_builder} and {file_wasm_test} have been merged into {file_merged}."
-#     )
-#     return
-
-# """
-# Retrieves the value of the specified command line option.
-# Args:
-#     option_name (str): The name of the option to retrieve.
-# Returns:
-#     str or None: The value of the option if found, or None if the option is not present.
-# """
-# def get_option(option_name):
-#     if option_name in sys.argv:
-#         index = sys.argv.index(option_name)
-#         try:
-#             return sys.argv[index + 1]
-#         except IndexError:
-#             print(f"Missing value for {option_name}")
-#             print_usage()
-#     return None
